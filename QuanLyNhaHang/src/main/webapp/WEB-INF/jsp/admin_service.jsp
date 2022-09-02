45ab3e344ae4d2bfb334e38ce8cca3c9536c7a24
giahuyoke <giahuyoke01@gmail.com>
2022-09-03 00:41:38 +0700

    Update admin ui

--- QuanLyNhaHang/src/main/webapp/WEB-INF/jsp/admin_service.jsp
+++ QuanLyNhaHang/src/main/webapp/WEB-INF/jsp/admin_service.jsp
@@ -18,19 +18,78 @@
         <br/>
         <br/>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="text-center col-md-7 mx-auto">
-                <h2 id="service-title" style="padding: 20px;"><spring:message key="order.service"/></h2>
-            </div>
-        </div>
-    </div>
+    <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#serviceModal" onclick="initModalService()" >
+        <spring:message key="admin.service.add"/>
+    </button>
     <hr class="mb-4"/>
+
     <div class="col-8 d-flex" id="order-services">
 
     </div>
 </div>
-<script src="<c:url value="/js/staff.js"/>"></script>
+<div class="modal fade" id="serviceModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body d-block" id="imageBox">
+                <form class="row">
+                    <div class="col-4">
+                        <img src="" id="img" class="d-block card-img-top" alt="upload file here"/>
+                        <input type="file" class="form-control-file" accept="image/png, image/gif, image/jpeg" id="imageUpload" style="padding: 5px;"/>
+                    </div>
+                    <div class="col-8">
+                        <div class="form-row">
+                            <div class="col">
+                                <label for="nameService"><spring:message key="admin.service.name"/></label>
+                                <input type="text" id="nameService" class="form-control" placeholder="<spring:message key="admin.service.typename"/>">
+                            </div>
+                            <div class="col">
+                                <label for="price"><spring:message key="admin.hall.price"/></label>
+                                <input type="number" min="0" step="10000" id="price" class="form-control" placeholder="<spring:message key="order.hall.price"/>">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="col">
+                                <label for="describe"><spring:message key="admin.hall.describe"/></label>
+                                <input type="text" id="describe" class="form-control" placeholder="<spring:message key="admin.hall.typedescribe"/>">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer d-flex">
+                <button type="button" id="btn-delete" class="btn btn-danger" ><spring:message key="admin.delete"/> </button>
+                <button type="button" class="btn btn-secondary"><spring:message key="admin.save"/></button>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    .form-control {
+        display: block;
+        width: 100%;
+        font-size: 0.875rem;
+        font-weight: 400;
+        line-height: 1;
+        color: #69707a;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #c5ccd6;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        border-radius: 0.35rem;
+        transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+        font-weight: bold;
+    }
+    label{
+        color: #16181b;
+        font-weight: bold;
+    }
+</style>
+<script src="<c:url value="/js/admin.js"/>"></script>
 <script>
     window.onload = function () {
         loadService(`${endpoint_service}`);
--- QuanLyNhaHang/src/main/webapp/resources/js/admin.js
+++ QuanLyNhaHang/src/main/webapp/resources/js/admin.js
@@ -1,25 +1,28 @@
-function editHall(name,describe,capacity,price) {
-    document.getElementById("nameHall").value=name;
-    document.getElementById("describeHall").value=describe;
-    document.getElementById("capacityHall").value=parseInt(capacity);
-    document.getElementById("priceHall").value=parseInt(price);
-    document.getElementById("deleteHall").style.display="block";
-    document.getElementById("modal-title").innerHTML="Chỉnh sửa sảnh";
+function editHall(name, describe, capacity, price) {
+    document.getElementById("nameHall").value = name;
+    document.getElementById("describeHall").value = describe;
+    document.getElementById("capacityHall").value = parseInt(capacity);
+    document.getElementById("priceHall").value = parseInt(price);
+    document.getElementById("deleteHall").style.display = "block";
+    document.getElementById("modal-title").innerHTML = "Chỉnh sửa sảnh";
 }
-function initModalEdit(){
-    document.getElementById("nameHall").value=null;
-    document.getElementById("describeHall").value=null;
-    document.getElementById("capacityHall").value=null;
-    document.getElementById("priceHall").value=null;
-    document.getElementById("deleteHall").style.display="none";
-    document.getElementById("modal-title").innerHTML="Thêm sảnh";
+
+function initModalEdit() {
+    document.getElementById("nameHall").value = null;
+    document.getElementById("describeHall").value = null;
+    document.getElementById("capacityHall").value = null;
+    document.getElementById("priceHall").value = null;
+    document.getElementById("deleteHall").style.display = "none";
+    document.getElementById("modal-title").innerHTML = "Thêm sảnh";
 }
-let count_dishById=0;
-let idByCateId=0
-let Page=0;
+
+let count_dishById = 0;
+let idByCateId = 0
+let Page = 0;
+
 function loadPage(endpoint, categoryId, page, pagesize, count) {
-    count_dishById=count;
-    idByCateId=categoryId;
+    count_dishById = count;
+    idByCateId = categoryId;
     let d = document.getElementById("table_dishes");
     d.innerHTML = `<thead class="thead-light">
             <tr>
@@ -33,7 +36,7 @@ function loadPage(endpoint, categoryId, page, pagesize, count) {
         return response.json();
     }).then(function (data) {
         data.forEach(dish => {
-            let image=`${dish.imgae}`;
+            let image = `${dish.imgae}`;
             d.innerHTML += `
             <tr class="table-info">
                 <th scope="row">${dish.id}</th>
@@ -49,27 +52,30 @@ function loadPage(endpoint, categoryId, page, pagesize, count) {
     for (let i = 1; i <= Math.ceil(count / pagesize); i++) {
         let newEnpoint = `${endpoint}`
         p.innerHTML += `<li class="page-item"><a type="button" class="page-link" href="#" onclick="loadPage(\`${newEnpoint}\`,${categoryId},${i},${pagesize},${count})">${i}</a></li>`
-        Page=page;
+        Page = page;
     }
 }
-function editDish(id, name, image ,  price, categoryId){
-    document.getElementById('categoryDish').value=categoryId;
-    document.getElementById("imageDish").src=image;
-    document.getElementById("nameDishes").value=name;
-    document.getElementById("price").value=parseInt(price);
-    document.getElementById("btn-delete").style.display="block";
+
+function editDish(id, name, image, price, categoryId) {
+    document.getElementById('categoryDish').value = categoryId;
+    document.getElementById("imageDish").src = image;
+    document.getElementById("nameDishes").value = name;
+    document.getElementById("price").value = parseInt(price);
+    document.getElementById("btn-delete").style.display = "block";
 }
-function initEditDishes(){
-    document.getElementById('categoryDish').value=1;
-    document.getElementById("imageDish").src="";
-    document.getElementById("nameDishes").value=null;
-    document.getElementById("price").value=0;
-    document.getElementById("btn-delete").style.display="none";
+
+function initEditDishes() {
+    document.getElementById('categoryDish').value = 1;
+    document.getElementById("imageDish").src = "";
+    document.getElementById("nameDishes").value = null;
+    document.getElementById("price").value = 0;
+    document.getElementById("btn-delete").style.display = "none";
 }
+
 function search_dish(endpoint, categoryId, page, pagesize, count) {
     let s = document.getElementById("search").value;
-    count=count_dishById;
-    categoryId=idByCateId;
+    count = count_dishById;
+    categoryId = idByCateId;
     console.log(s.length);
     if (s.length !== 0) {
         let d = document.getElementById("table_dishes");
@@ -87,7 +93,7 @@ function search_dish(endpoint, categoryId, page, pagesize, count) {
             }).then(function (data) {
                 data.forEach(dish => {
                     if (removeAccents(dish.name).toLowerCase().indexOf(removeAccents(s).toLowerCase().trimEnd()) !== -1) {
-                        let image=`${dish.imgae}`;
+                        let image = `${dish.imgae}`;
                         d.innerHTML += `
             <tr class="table-info">
                 <th scope="row">${dish.id}</th>
@@ -100,11 +106,11 @@ function search_dish(endpoint, categoryId, page, pagesize, count) {
                 })
             })
         }
-    }
-    else {
+    } else {
         loadPage(endpoint, categoryId, Page, pagesize, count);
     }
 }
+
 function removeAccents(str) {
     let AccentsMap = [
         "aàảãáạăằẳẵắặâầẩẫấậ",
@@ -121,21 +127,23 @@ function removeAccents(str) {
         "yỳỷỹýỵ",
         "YỲỶỸÝỴ"
     ];
-    for (let i=0; i<AccentsMap.length; i++) {
+    for (let i = 0; i < AccentsMap.length; i++) {
         let re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
         let char = AccentsMap[i][0];
         str = str.replace(re, char);
     }
     return str;
 }
+
 function loadService(endpoint) {
     fetch(endpoint + `?page=1`).then(function (response) {
         return response.json();
     }).then(function (data) {
         let msg = document.getElementById("order-services");
-        msg.innerHTML=``;
+        msg.innerHTML = ``;
         data.forEach(item => {
-            msg.innerHTML +=`
+            let image=`${item.image}`
+            msg.innerHTML += `
                         <div class="col-md-3 col-xs-12 d-flex" style="padding: 10px;">
                                 <div class="card" id="div-${item.id}">
                                     <img class="card-img-top" class="img-fluid" src="${item.image}" alt="Card image">
@@ -145,10 +153,27 @@ function loadService(endpoint) {
                                         <p class="card-text">
                                                 ${item.price},000 [VND]
                                         </p>
-                                    </div>                                    
+                                    </div>       
+                                    <div class="card-footer d-flex justify-content-center align-items-center">
+                                        <button type="button" data-bs-toggle="modal" data-bs-target="#serviceModal" onclick="editService(\`${item.name}\`,\`${item.price}\`,\`${item.describe}\`,\`${image}\`)" class="btn btn-info">Chỉnh sửa</button>
+                                     </div>                             
                                 </div>
                             </div>
                 `
         })
     })
+}
+function editService(name,price,describe,img){
+    document.getElementById("img").src=img;
+    document.getElementById("nameService").value=name;
+    document.getElementById("describe").value=describe;
+    document.getElementById("price").value=parseInt(price);
+    document.getElementById("btn-delete").style.display="block";
+}
+function initModalService(){
+    document.getElementById("img").src='';
+    document.getElementById("nameService").value=null;
+    document.getElementById("describe").value=null;
+    document.getElementById("price").value=0;
+    document.getElementById("btn-delete").style.display="none";
 }
